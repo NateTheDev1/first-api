176aa56fe9e0a430aeb67924415f97ab517676f3
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-16 00:26:24 -0400

    Can find a post by id, get all posts, and delete a post

--- routes/posts.js
+++ routes/posts.js
@@ -2,21 +2,64 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("We are on posts");
+// GETS ALL POSTS
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-router.post("/", (req, res) => {
+// SUBMITS A POST
+
+// router.post("/", (req, res) => {
+//   const post = new Post({
+//     title: req.body.title,
+//     description: req.body.description,
+//   });
+//   post
+//     .save()
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((err) => res.json(err));
+// });
+
+// ALSO FINE BELOW
+router.post("/", async (req, res) => {
   const post = new Post({
     title: req.body.title,
     description: req.body.description,
   });
-  post
-    .save()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => res.json(err));
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// FIND POST BY ID
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// DELETE POST
+
+router.delete("/:postId", async (req, res) => {
+  try {
+    const res = await Post.remove({ _id: req.params.postId });
+    res.json(res);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
